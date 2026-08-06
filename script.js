47f038a92afbcf6afd3aad66f38ed76b6724b199
yuyutacomsono195-code <yuyutacomsono195@gmail.com>
2026-08-06 01:50:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function verificar(resposta) {
+    const resultado = document.getElementById('resultado-final');
+    if(resposta === 'jardineiro') {
+        resultado.className = 'resultado correto';
+        resultado.innerHTML = "✅ Correto! Ninguém corta grama na chuva. Ele mentiu!";
+    } else {
+        resultado.className = 'resultado errado';
+        resultado.innerHTML = "❌ Errado! Pense novamente na desculpa de cada um.";
+    }
+}
 
